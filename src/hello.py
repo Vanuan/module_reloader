71179d5c3eebe155eb1068c8a76cf89b7d9c58d5
John Yani <vanuan@gmail.com>
2012-10-26 02:44:53 +0300

    Functin module reloader

---
+++ src/hello.py
@@ -0,0 +1,5 @@
+from module_reloader import reloader 
+reloader.reloadModifiedModules()
+
+import module_to_be_reloaded
+module_to_be_reloaded.test()
---
+++ src/module_reloader/reloader.py
@@ -0,0 +1,66 @@
+'''
+The purpose of this module is to reload modules that were modified
+after the last reload.
+
+It uses import hook to determine a module file timestamp during import/reload
+
+reloadModifiedModules function determines current timestamp
+and reloads a module it is was changed
+
+== Usage:
+
+from module_reloader import reloader 
+reloader.reloadModifiedModules()
+
+@author: Vanuan
+'''
+import os, time, types, sys
+
+import _import_hook # import only once, do not reload
+
+def reloadModifiedModules(): 
+    '''
+    Call this every time you run a Jython script.
+
+    Reloads only modules that are modified.
+    '''
+    reloadModulesWhere(condition=__isModified) 
+
+
+def reloadAllModules():
+    reloadModulesWhere(condition=lambda moduleName: True)
+
+def reloadModulesWhere(condition=lambda moduleName: True):
+    reload(sys.modules[__name__])
+    start = time.time()
+    print "Reloading modules...\n[\n",
+    for modulename, module in sys.modules.iteritems():
+        if (modulename == '__main__' or
+            module == None or
+            modulename == 're' or modulename == '__builtin__' or
+            modulename == 'sre_constants' # raises exception if it is reloaded
+                                          # and re.compile was used
+            ):
+            pass
+        elif isinstance(module, types.ModuleType):
+            if condition(modulename):
+                reload(module)
+        else:
+            pass
+    print "\n]\nDone in", round(time.time() - start, 2), "seconds."
+
+
+def __isModified(moduleName):
+    '''
+    Determines whether a module was modified after the last reload
+    '''
+    if moduleName in _import_hook.global_modules_timestamps:
+        moduleFileName, oldModuleMtime = \
+                _import_hook.global_modules_timestamps[moduleName]
+        currentModuleMtime = time.ctime(os.path.getmtime(moduleFileName))
+        condition = oldModuleMtime != currentModuleMtime
+        if condition:
+            print "... reloading '" + moduleName + "'",
+        return condition
+    else: 
+        return False
